3b5892857fc8cd4eddcfb423f1c4aa122d901e1f
Hakim Teasdell <4keem@Hakims-Mac-mini.local>
2023-06-30 21:56:41 -0400

    Started Front & Backend

---
+++ server/src/test/java/project/htjobboard/HtJobBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package project.htjobboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtJobBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
