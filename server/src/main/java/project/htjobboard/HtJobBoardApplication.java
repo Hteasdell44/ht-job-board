1e5b60f6c87c6da288fc2a8d8244430efcbe7e60
Hakim Teasdell <4keem@Hakims-Mac-mini.local>
2023-07-13 00:16:47 -0400

    v71

--- server/src/main/java/project/htjobboard/HtJobBoardApplication.java
+++ server/src/main/java/project/htjobboard/HtJobBoardApplication.java
@@ -12,7 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Configuration
+@EnableWebMvc
 @SpringBootApplication
 @ComponentScan("project.htjobboard")
 public class HtJobBoardApplication {
