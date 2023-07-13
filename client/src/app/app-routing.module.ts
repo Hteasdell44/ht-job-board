e95d901032d6e3b2186a7ede2b0eba0fd58bfa62
Hakim Teasdell <4keem@Hakims-Mac-mini.local>
2023-07-13 15:57:44 -0400

    v88

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -14,8 +14,8 @@ import { AddJobComponent } from './add-job/add-job.component';
 import { CompanyDetailsComponent } from './company-details/company-details.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'signup', component: ApplicantSignupComponent },
+  { path: '/', component: HomeComponent },
+  { path: '/signup', component: ApplicantSignupComponent },
   { path: 'login', component: ApplicantLoginComponent },
   { path: 'profile', component: ProfileComponent },    
   { path: 'jobs/:id', component: JobDetailsComponent },
