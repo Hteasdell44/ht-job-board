a24755d6cea510f08b09dff6748ce0b2c1f50db9
Hakim Teasdell <4keem@Hakims-Mac-mini.local>
2023-07-13 17:37:16 -0400

    v97

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -15,8 +15,6 @@ import { CompanyDetailsComponent } from './company-details/company-details.compo
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'dummy', component: HomeComponent },
-
   { path: 'signup', component: ApplicantSignupComponent },
   { path: 'login', component: ApplicantLoginComponent },
   { path: 'profile', component: ProfileComponent },    
